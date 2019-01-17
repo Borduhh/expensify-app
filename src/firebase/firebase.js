83f07ef6ee1e66ec2cf9a37ce996802d57097920
Nicholas Bordeau <nrb@NRB-MBP.local>
2019-01-17 14:03:04 -0500

    Added environment variables and test Firebase Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyDJ3WJSDsEkPZ0jyM84PfEqOHDrvN_tMw8",
-  authDomain: "expensify-9de38.firebaseapp.com",
-  databaseURL: "https://expensify-9de38.firebaseio.com",
-  projectId: "expensify-9de38",
-  storageBucket: "expensify-9de38.appspot.com",
-  messagingSenderId: "130744941575"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
