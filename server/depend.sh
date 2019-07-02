4a270a1561de50341aa80e6ff126c3625701b618
Nick Bordeau <nbordeau@pipusa.com>
2019-07-02 16:28:33 -0400

    Changed webpack to dev mode to test on AWS

--- server/depend.sh
+++ server/depend.sh
@@ -2,4 +2,4 @@
 
 cd /home/ec2-user/expensify
 sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm install
-sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build:prod
+sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build:dev
