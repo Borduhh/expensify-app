a339adc1fb9e4d1684b900a27f62bd141eab830a
Nick Bordeau <nbordeau@pipusa.com>
2019-07-02 16:39:00 -0400

    Added workaround for node-sass error

--- server/depend.sh
+++ server/depend.sh
@@ -2,4 +2,9 @@
 
 cd /home/ec2-user/expensify
 sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm install
+
+# Workaround for Node-Sass error
+# https://github.com/sass/node-sass/issues/1579
+sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm rebuild node-sass
+
 sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build:dev
