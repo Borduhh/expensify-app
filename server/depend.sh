8e549fe67bc329c23c64e51faeb369e150894c3e
Nick Bordeau <nbordeau@pipusa.com>
2019-07-02 16:43:26 -0400

    Switched back to prod build since dev also hangs.

--- server/depend.sh
+++ server/depend.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd /home/ec2-user/expensify
+
 sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm install
 
 # Workaround for Node-Sass error
 # https://github.com/sass/node-sass/issues/1579
 sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm rebuild node-sass
 
-sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build:dev
+sudo /home/ec2-user/.nvm/versions/node/v10.16.0/bin/npm run build:prod
