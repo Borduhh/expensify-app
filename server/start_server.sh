18736303afa085f14e55f82ff0718579ddd3870c
Nick Bordeau <nbordeau@pipusa.com>
2019-07-02 14:27:17 -0400

    Added path to npm command.

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,5 +1,4 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#!/bin/bash
 
-node server/server.js
+cd ~/expensify
+~/.nvm/versions/node/v10.16.0/bin/npm start
